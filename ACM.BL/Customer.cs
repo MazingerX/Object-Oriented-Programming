9dda243f3a877a6f2e7fd86905131a0357f00dd5
mazingerx <lehinostroza@icloud.com>
2019-10-01 21:35:21 -0500

    Code with OOP application and xtest code

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,30 @@
+using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+
+            // -- Arrange
+            Customer customer = new Customer()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
